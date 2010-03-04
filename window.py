ddddc639be455be03a3270c9e58a34e6c2bbc7e0
M. Nasimul Haque <nasim.haque@gmail.com>
2010-03-04 19:38:46 +0000

    plot graph

--- window.py
+++ window.py
@@ -33,16 +33,19 @@ class MainWindow(QtGui.QDialog):
         self.date_picker.setDate(QtCore.QDate.currentDate())
         date_label.setBuddy(self.date_picker)
 
+        ind_label = QtGui.QLabel("Indicator:")
+        self.ind_combo = QtGui.QComboBox()
+        ind_label.setBuddy(self.ind_combo)
+
+        self.graph_button = QtGui.QPushButton("Graph data")
         self.fetch_button = QtGui.QPushButton("Start fetching")
         self.stop_button = QtGui.QPushButton("Stop fetching")
         self.stop_button.setEnabled(False)
 
-        self.canvas = QtGui.QGraphicsView()
-        self.status = QtGui.QLabel("Ready")
+        self.scene = QtGui.QGraphicsScene(0, 0, 700, 320);
+        canvas = QtGui.QGraphicsView(self.scene)
 
-        ind_label = QtGui.QLabel("Indicator:")
-        self.ind_combo = QtGui.QComboBox()
-        ind_label.setBuddy(self.ind_combo)
+        self.status = QtGui.QLabel("Ready")
 
         hbox_trade = QtGui.QHBoxLayout()
         hbox_trade.addWidget(trade_label)
@@ -52,24 +55,37 @@ class MainWindow(QtGui.QDialog):
         hbox_date.addWidget(date_label)
         hbox_date.addWidget(self.date_picker)
 
+        vbox1 = QtGui.QVBoxLayout()
+        vbox1.addLayout(hbox_trade)
+        vbox1.addLayout(hbox_date)
+
         hbox_ind = QtGui.QHBoxLayout()
         hbox_ind.addWidget(ind_label)
         hbox_ind.addWidget(self.ind_combo)
         hbox_ind.addStretch()
 
-        hbox_buttons = QtGui.QHBoxLayout()
-        hbox_buttons.addWidget(self.fetch_button)
-        hbox_buttons.addWidget(self.stop_button)
+        hbox_graph = QtGui.QHBoxLayout()
+        hbox_graph.addWidget(self.graph_button)
+        hbox_graph.addStretch()
+
+        vbox2 = QtGui.QVBoxLayout()
+        vbox2.addLayout(hbox_ind)
+        vbox2.addLayout(hbox_graph)
+
+        vbox_buttons = QtGui.QVBoxLayout()
+        vbox_buttons.addWidget(self.fetch_button)
+        vbox_buttons.addWidget(self.stop_button)
 
         grid = QtGui.QGridLayout(self)
-        grid.addLayout(hbox_trade, 0, 0)
-        grid.addLayout(hbox_date, 0, 1)
-        grid.addLayout(hbox_ind, 0, 2)
-        grid.addLayout(hbox_buttons, 0, 3)
-        grid.addWidget(self.canvas, 1, 0, 1, 4)
+        grid.addLayout(vbox1, 0, 0)
+        grid.addLayout(vbox2, 0, 1)
+        grid.addLayout(vbox_buttons, 0, 3)
+        grid.addWidget(canvas, 1, 0, 1, 4)
         grid.addWidget(self.status, 2, 0, 1, 4)
 
     def create_connections(self):
+        self.connect(self.graph_button, QtCore.SIGNAL("clicked()"),
+                     self.plot_graph)
         self.connect(self.fetch_button, QtCore.SIGNAL("clicked()"),
                      self.start_fetching)
         self.connect(self.stop_button, QtCore.SIGNAL("clicked()"),
@@ -153,4 +169,86 @@ class MainWindow(QtGui.QDialog):
                       trade=int(d[9]), volume=int(d[10]))
 
             session.commit()
+
+    def load_data(self):
+        dirname = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                               'csv')
+        for dir, dirs, fnames in os.walk(dirname):
+            for f in fnames:
+                if f.endswith(".csv"):
+                    fname = os.path.join(dir, f)
+                    trade_at = datetime.datetime.strptime(fname[-23:-4],
+                                                          "%Y-%m-%dT%H-%M-%S")
+
+                    with open(fname, 'r') as csv:
+                        self.save_data(trade_at, csv.read())
+
+                    print("%s: %d" % (fname, Trade.query.count()))
+
+    def plot_graph(self):
+        code = Code.get_by(code=unicode(self.trade_combo.currentText()))
+        indicator = unicode(self.ind_combo.currentText())
+        date = self.date_picker.date()
+        date = datetime.datetime(day=date.day(), month=date.month(),
+                                 year=date.year())
+
+        trades = Trade.get_by_code(code, date).order_by(Trade.trade_at).all()
+
+        if len(trades) == 0:
+            self.scene.clear()
+            self.scene.addText("No data available")
+            return
+
+        indexes = {
+            'Open': lambda x: x.open,
+            'Close': lambda x: x.close,
+            'Trade': lambda x: x.trade,
+            'Volume': lambda x: x.volume,
+        }
+
+        day_start = 7 * 60
+        day_end = 18 * 60
+
+        times = []
+        index = []
+        for trade in trades:
+            times.append(trade.trade_at.minute + trade.trade_at.hour * 60 -
+                         day_start)
+            index.append(indexes[indicator](trade))
+
+        max_index = max(index)
+        min_index = min(index)
+        if max_index == min_index:
+            min_index = 0
+        points = zip(times, map(lambda x: 280-270*(x-min_index)/max_index,
+                                index))
+
+        path = QtGui.QPainterPath()
+        path.moveTo(points[0][0], points[0][1])
+        for p in points:
+            path.lineTo(p[0], p[1])
+
+        self.scene.clear()
+        self.draw_time_axis()
+        self.draw_value_axis(min_index, max_index)
+        self.scene.addPath(path)
+
+    def draw_time_axis(self):
+        self.scene.addLine(QtCore.QLineF(-50, 290, 700, 290))
+        for i in range(20, 681, 60):
+            self.scene.addLine(i, 280, i, 290)
+            text = QtGui.QGraphicsTextItem("%02d:00" % (i/60 + 7))
+            text.setPos(i-20, 295)
+            self.scene.addItem(text)
+
+    def draw_value_axis(self, minx, maxx):
+        self.scene.addLine(QtCore.QLineF(20, -10, 20, 330))
+
+        dx = (maxx - minx) / 9.0
+        for i in range(280, 0, -30):
+            self.scene.addLine(20, i, 30, i)
+            text = QtGui.QGraphicsTextItem("%.0f" % (dx*(280-i)/30))
+            text.setPos(30, i-10)
+            self.scene.addItem(text)
+
 
